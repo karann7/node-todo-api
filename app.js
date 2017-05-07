64635b764077a774e6b60945ffeb973200a9b542
Karan Singh <kar.n@icloud.com>
2017-05-06 19:55:50 -0700

    added different env variables depending on what environment the server is running in

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 "use strict";
 /////***DEPENDENCIES***/////
+
 //local
+require('./config/config');
 const mongoose 		= require('./db/mongoose'),
 			User     		= require('./db/models/user'),
 			Todo     		= require('./db/models/todo'),
-			port				= process.env.PORT || 3000,
-			ip 					= process.env.IP || '0.0.0.0';
+			port				= process.env.PORT;
 
 //npm
 const express  		= require('express'),
@@ -82,25 +83,6 @@ app.post('/users', (req, res)=>{
 
 /////PUT ROUTES/////
 
-//Find a todo by ID and update it
-// app.put('/todos/:id', (req, res)=>{
-// 	let id = req.params.id;
-// 	let text = req.body.text;
-// 	if(!ObjectID.isValid(id)) {
-// 		return res.status(404).send("ID is not valid!");
-// 	}
-// 	Todo.findById(id).then((todo)=>{
-// 		if(!todo){
-// 			res.status(404).send('That Todo does not exist');
-// 		} else {
-
-// 			res.status(200).send({todo});
-// 		  }
-// 		}).catch((e)=>{
-// 			res.status(400).send('An error has occured.');
-// 		});
-// });
-
 app.patch('/todos/:id', (req, res)=>{
 	let id = req.params.id;
 	let body = _.pick(req.body, ['text', 'completed']);
@@ -146,7 +128,7 @@ app.delete('/todos/:id', (req, res)=>{
 });
 
 ////Server Listening////
-app.listen(port, ip, ()=>{
+app.listen(port, ()=>{
 	console.log(`Server is running on Port: ${port}`);
 });
 
